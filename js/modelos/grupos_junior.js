ea63cdc252c538b4c725d869d43b5ee1c21591c3
Mateus Cardoso <cardoso.olim@gmail.com>
2025-02-25 14:22:06 -0300

    att 25/02

--- js/modelos/grupos_junior.js
+++ js/modelos/grupos_junior.js
@@ -6,9 +6,10 @@ const caiboate = new Grupo('01', 'CTG Caiboaté', '18', 'São Gabriel');
 const chaleira_preta = new Grupo('24', 'GF Chaleira Preta', '9', 'Ijuí');
 const tropilha_crioula = new Grupo('28', 'CTG Tropilha Crioula', '19', 'Getúlio Vargas');
 const palanques_da_tradição = new Grupo('29', 'CTG Palanques da Tradição', '30', 'Campo Bom');
+const tropeiros_da_amizade = new Grupo('42', 'CTG Tropeiros da Amizade', '21', 'Canguçu');
 
 //Lista com os grupos cabeça de chave
-export let lista_cabeça = [caiboate, chaleira_preta, tropilha_crioula, palanques_da_tradição];
+export let lista_cabeça = [caiboate, chaleira_preta, tropilha_crioula, palanques_da_tradição, tropeiros_da_amizade];
 
 //Grupos geral (Meu Deus...)
 const polivalente = new Grupo('02','DTG Polivalente','2','São Jerônimo');
@@ -33,11 +34,21 @@ const lenco_preto = new Grupo('20','CTG Lenço Preto','19','Trindade do Sul');
 const ultima_tropeada = new Grupo('21','CTG Última Tropeada','11','Guapore');
 const laco_velho = new Grupo('22','CTG Laço Velho','11','Bento Gonçalves');
 const sentinela_da_serra = new Grupo('23','CTG Sentinela da Serra','11','Garibaldi');
-const retorno_a_querencia = new Grupo('25','CTG Retorno a Querência','11','Nova Prata');
 const chama_nativa = new Grupo('26','CTG Chama Nativa','12','Esteio');
 const jose_bonifacio = new Grupo('30','CTG José Bonifácio Gomes','12','São Leopoldo');
+const irmaos_sagrilo = new Grupo('33','PT Irmãos Sagrilo','10','Santiago');
+const retorno_a_querencia = new Grupo('34','CTG Retorno a Querência','11','Nova Prata');
+const essencia_da_tradicao = new Grupo('35','GF Essência da Tradição','24','Venancio Aires');
+const sentinela_dos_sinos = new Grupo('36','CTG Sentinela dos Sinos','1','Caraa');
+const doze_bracas = new Grupo('37','CTG Doze Braças','29','Sananduva');
+const rancho_de_gauderios = new Grupo('38','CTG Rancho de Gaudérios','25','Farroupilha');
+const querencia_da_medianeira = new Grupo('40','DT Querência da Medianeira','13','Santa Maria');
+const porteira_da_tradição = new Grupo('41','CTG Porteira da Tradição','1','Eldorado do Sul');
+const rancho_amigo = new Grupo('43','CTG Rancho Amigo','19','Barão de Cotegipe');
+const carreteiros_da_saudade = new Grupo('44','CTG Carreteiros da Saudade','5','Pantano Grande');
+const rancho_dos_tropeiros = new Grupo('45','CTG Rancho dos Tropeiros','9','Ibiruba');
 
 //Lista dos grupos geral (MEU DEUS...)
-export let lista_geral = [polivalente, adaga_velha, timbauva, os_vaqueanos, sinuelo_dos_ervais, sinuelo, os_legalistas, cftg_farroupilha, sentinelas_da_tradição, galpao_do_chiru, lanceiros_do_mar, querencia_do_imbe, rincao_da_carolina, galpao_da_boa_vontade, coracao_do_rio_grande, rodeio_da_fronteira, dom_luiz, manoel_teixeira, lenco_preto, ultima_tropeada, laco_velho, sentinela_da_serra, retorno_a_querencia, chama_nativa, jose_bonifacio];
+export let lista_geral = [polivalente, adaga_velha, timbauva, os_vaqueanos, sinuelo_dos_ervais, sinuelo, os_legalistas, cftg_farroupilha, sentinelas_da_tradição, galpao_do_chiru, lanceiros_do_mar, querencia_do_imbe, rincao_da_carolina, galpao_da_boa_vontade, coracao_do_rio_grande, rodeio_da_fronteira, dom_luiz, manoel_teixeira, lenco_preto, ultima_tropeada, laco_velho, sentinela_da_serra, retorno_a_querencia, chama_nativa, jose_bonifacio, irmaos_sagrilo, essencia_da_tradicao, sentinela_dos_sinos, doze_bracas, rancho_de_gauderios, querencia_da_medianeira, porteira_da_tradição, rancho_amigo, carreteiros_da_saudade, rancho_dos_tropeiros];
 
 export let lista_total = lista_cabeça.concat(lista_geral)
--- js/modelos/grupos_senior.js
+++ js/modelos/grupos_senior.js
@@ -11,9 +11,10 @@ const aldeia_farroupilha = new GrupoC('11','CTG Aldeia Farroupilha','25','Farrou
 const patrulha_do_rio_grande = new GrupoC('12','CTG Patrulha do Rio Grande','23','Santo Antônio da Patrulha');
 const cftg = new GrupoC('17','Centro Farroupilha de Tradições Gaúchas','4','Alegrete');
 const herois_farroupilhas = new GrupoC('18','CTG Heróis Farroupilhas','25','Caxias do Sul');
+const alma_gaucha_13 = new GrupoC('20','CTF Alma Gaúcha','13','Santa Maria');
 
 //Lista com os cabeças de chave
-export const lista_cabeca = [pia_do_sul, estancia_da_serra, aldeia_farroupilha, patrulha_do_rio_grande, cftg, herois_farroupilhas];
+export const lista_cabeca = [pia_do_sul, estancia_da_serra, aldeia_farroupilha, patrulha_do_rio_grande, cftg, herois_farroupilhas, alma_gaucha_13];
 
 //GRUPOS GERAL
 const felipe_portinho = new Grupo('02','CTG Felipe Portinho','7','Marau');
@@ -26,9 +27,10 @@ const alma_gaucha_18 = new Grupo('13','DTG DC Alma Gaúcha','18','Dom Pedrito');
 const guapos_do_itapui = new Grupo('14','CTG Guapos do Itapuí','30','Campo Bom');
 const amanhecer_na_querencia = new Grupo('15','CTG Amanhecer na Querência','1','Alvorada');
 const velha_carreta = new Grupo('16','GTCN Velha Carreta','25','Caxias do Sul');
+const mborore = new Grupo('19',"CTG M'bororé",'30','Campo Bom');
 
 //Lista com os grupos geral
-export let lista_geral = [felipe_portinho, camboata, os_farrapos, gt_sepe_tiaraju, charrua, lalau_miranda, alma_gaucha_18, guapos_do_itapui, amanhecer_na_querencia, velha_carreta];
+export let lista_geral = [felipe_portinho, camboata, os_farrapos, gt_sepe_tiaraju, charrua, lalau_miranda, alma_gaucha_18, guapos_do_itapui, amanhecer_na_querencia, velha_carreta, mborore];
 
 let arrayProvisorio = campeao_anterior.concat(lista_cabeca);
 
